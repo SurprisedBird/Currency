0bce1f655cdea26f95b5e14535ad91166c38d28c
SurprisedBird <irinayavors@gmail.com>
2021-07-15 11:49:24 +0300

    Created django app and contact us model

---
+++ my_app/currency/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class ContactUs(models.Model):
+    name = models.CharField(max_length=15)
+    email = models.EmailField()
+    text = models.CharField(max_length=300)
+    phone_number = models.PositiveIntegerField(max_length=12)
+    date = models.DateField()
+    time = models.TimeField()
+    file = models.FileField()
