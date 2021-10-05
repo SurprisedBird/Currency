fe5f2685048ccb28409de595f115e450bdf9cfc4
SurprisedBird <irinayavors@gmail.com>
2021-10-05 11:02:16 +0300

    Pytest basics

--- my_app/tests/test_contactus.py
+++ my_app/tests/test_contactus.py
@@ -1,3 +1,4 @@
+from currency.models import ContactUs
 
 URL = '/currency/contactus/create/'
 
@@ -8,6 +9,7 @@ def test_get_contactus(client):
 
 
 def test_post_empty_form(client):
+    contactus_initial_count = ContactUs.objects.count()
     response = client.post(URL, data={})
     assert response.status_code == 200
     assert response.context_data['form'].errors == {
@@ -15,10 +17,11 @@ def test_post_empty_form(client):
         'subject': ['This field is required.'],
         'body': ['This field is required.'],
     }
-    # assert ContactUs.objects.count() == contactus_initial_count
+    assert ContactUs.objects.count() == contactus_initial_count
 
 
 def test_invalid_form(client):
+    contactus_initial_count = ContactUs.objects.count()
     form_data = {
         'email_to': 'test_invalid_form',
         'subject': 'test_subject'*100,
@@ -30,9 +33,11 @@ def test_invalid_form(client):
     assert response.context_data['form'].errors == {
         'email_to': ['Enter a valid email address.'],
         'subject': ['Ensure this value has at most 255 characters (it has 1200).'], }
+    assert ContactUs.objects.count() == contactus_initial_count
 
 
 def test_valid_form(client):
+    contactus_initial_count = ContactUs.objects.count()
     form_data = {
         'email_to': 'test_valid_form@ex.com',
         'subject': 'test_subject',
@@ -41,3 +46,9 @@ def test_valid_form(client):
 
     response = client.post(URL, data=form_data)
     assert response.status_code == 302
+    assert response.url == "/index/"
+    assert ContactUs.objects.count() == contactus_initial_count + 1
+    contact_us_object = ContactUs.objects.last()
+    assert contact_us_object.email_to == form_data['email_to']
+    assert contact_us_object.subject == form_data['subject']
+    assert contact_us_object.body == form_data['body']
