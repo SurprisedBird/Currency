0908293328a137f83187cd8d42ab9c32d1b72b93
SurprisedBird <irinayavors@gmail.com>
2021-10-26 09:51:17 +0300

    Added privat parser

--- my_app/currency/models.py
+++ my_app/currency/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 from currency import model_choices
 
@@ -20,7 +21,7 @@ class Source(models.Model):
 class Rate(models.Model):
     sale = models.DecimalField(max_digits=5, decimal_places=2)
     buy = models.DecimalField(max_digits=5, decimal_places=2)
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(default=timezone.now())
     source = models.ForeignKey(Source, related_name='rates', on_delete=models.CASCADE)
     curr_type = models.CharField(max_length=3, choices=model_choices.RATE_TYPES,
                                  blank=False, null=False, default=model_choices.TYPE_USD)
